7ff85344afb005e0c06f8ddb8d9b2a69357e3925
zusmo <josue.leoz10@gmail.com>
2020-06-03 23:07:21 -0600

    Third Commit

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
-class persona{
-    
+class user{
+    obtener(){
+        var email = document.getElementById("email").value;
+        var password = document.getElementById("password").value;
+
+        this.p(email, password);
+    }
+
+    p = function(email, password){
+        function verificar(){
+            if( email == "zusmo" && password == "1234"){
+                alert("Bienvenido " + email);
+            }else{
+                alert("Usuario Invalido");  
+            }
+        }
+        verificar();
+    };
 }
+
+var eject = new user();
